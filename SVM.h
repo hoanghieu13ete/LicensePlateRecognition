de94bf1a6fece70c159d6e19f3a592b7633ba152
Huy van <bnhuyvan0505@gmail.com>
2018-01-23 23:54:50 +0700

    11:54 - 23/1/2018

--- SVM.h
+++ SVM.h
@@ -1,1 +1,20 @@
+//// chay ham training de train mo hinh (can tap co so du lieu)
+/// chay ham testing de du doan, input can dinh danh: vector<Mat>, moi Mat co kich thuoc 12x24
+
 #pragma once
+#include <iostream>
+#include <string.h>
+#include <opencv2/ml.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include "opencv2/objdetect.hpp"
+
+using namespace cv::ml;
+using namespace cv;
+using namespace std;
+
+class SVMModel {
+public:
+	void training();
+	Mat testing(vector<Mat> &testCells);
+}; 
