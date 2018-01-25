b5eafb809e59a7a8a4ea17bf119ee3a99dbb327f
Hieu <Hieu@DESKTOP-QBGD79V>
2018-01-25 15:14:16 +0700

    15:14 25/1/2018

--- HaarCascade.cpp
+++ HaarCascade.cpp
@@ -39,36 +39,88 @@ Mat HaarCascade::DectectLicensePlate(Mat image)
 	return image;
 }
 
-vector<Mat> HaarCascade::DetectNumber(Mat image)
+vector<Rect> HaarCascade::DetectRegion(Mat image)
 {
-	vector<Mat> number;
-
 	Mat binary;
-
+	vector<Rect> region;
 	adaptiveThreshold(image, binary, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 35, 5);
 
 	vector<vector<Point>> countours1;
 
 	findContours(binary, countours1, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
 
-	/*imshow("binary", binary);
-	waitKey(0);*/
-
 	//size of a number 20x50.
 	for (size_t i = 0; i < countours1.size(); i++)
 	{
 		Rect r = boundingRect(countours1[i]);
 
-
 		if (r.width > 10 && r.width < 30 && r.height > 40 && r.height < 60)
 		{
-			Mat out;
-			cout << r.x << " " << r.y << " " << endl;
-			// size number to recognite: 12x24
-			resize(image(r), out, Size(12, 24));
-			number.push_back(out);
+			region.push_back(r);
 		}
 	}
+	return region;
+}
 
+bool wayToSort(Rect x, Rect y)
+{
+	return x.x < y.x;
+}
+
+vector<Rect> SortRect(vector<Rect> x)
+{
+	sort(x.begin(), x.end(), wayToSort);
+	return x;
+}
+
+// oder position 2 rows rect.
+vector<vector<Rect>> HaarCascade::OrderPostition(vector<Rect> region)
+{
+	vector<vector<Rect>> number;
+	vector<Rect> x1;
+	vector<Rect> x2;
+	for (int i = 0; i < region.size(); i++)
+	{
+		if (region.at(i).y < 50)
+		{
+			x1.push_back(region.at(i));
+		}
+		else if(region.at(i).y < 100)
+		{
+			x2.push_back(region.at(i));
+		}
+	}
+	x1 = SortRect(x1);
+	x2 = SortRect(x2);
+	number.push_back(x1);
+	number.push_back(x2);
 	return number;
+}
+
+//dectect 2 rows number image.
+vector<vector<Mat>> HaarCascade::DetectNumber(vector<vector<Rect>> number, Mat image)
+{
+	vector<vector<Mat>> numberDetect;
+	vector<Mat> row1;
+	vector<Mat> row2;
+	Mat out;
+
+	for (int j = 0; j < number[0].size(); j++)
+	{
+		Mat out = image(number[0][j]);
+		resize(out, out, Size(12, 24));
+		row1.push_back(out);
+	}
+
+	for (int j = 0; j < number[1].size(); j++)
+	{
+		Mat out = image(number[1][j]);
+		resize(out, out, Size(12, 24));
+		row2.push_back(out);
+	}
+
+	numberDetect.push_back(row1);
+	numberDetect.push_back(row2);
+
+	return numberDetect;
 }
--- HaarCascade.h
+++ HaarCascade.h
@@ -12,7 +12,9 @@ class HaarCascade
 public:
 	bool LoadXML();
 	Mat DectectLicensePlate(Mat image);
-	vector<Mat> DetectNumber(Mat image);
+	vector<Rect> DetectRegion(Mat image);
+	vector<vector<Rect>> OrderPostition(vector<Rect> region);
+	vector<vector<Mat>> HaarCascade::DetectNumber(vector<vector<Rect>> number, Mat image);
 private:
 	CascadeClassifier myDetect;
 };
