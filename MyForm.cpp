e50b5cb771f4b22fb2fbdf34e9bda19dde130194
HCD-Fresher021 <HCD-Fresher021@300-FRS-V121>
2018-02-07 09:56:11 +0700

    abc

--- MyForm.cpp
+++ MyForm.cpp
@@ -15,10 +15,10 @@ void main()
 	ifstream read;
 	myHaarCascade.LoadXML();
 	string dir, line;
-	read.open("D:/FPT learn/Haar Training/testingdata/bg.txt");
+	read.open("testingdata/bg.txt");
 	while (getline(read, line))
 	{
-		dir = "D:/FPT learn/Haar Training/testingdata/";
+		dir = "testingdata/";
 		dir.append(line);
 		cout << dir << endl;
 		image = imread(dir, CV_LOAD_IMAGE_GRAYSCALE);
@@ -28,20 +28,20 @@ void main()
 			Mat licensePlate = myHaarCascade.DectectLicensePlate(image, region);
 			if (licensePlate.cols == 150 && licensePlate.rows == 150)
 			{
-				vector<Rect> region = myHaarCascade.DetectRegion(licensePlate);
 				vector<vector<Rect>> Position = myHaarCascade.OrderPostition(region);
 				vector<vector<Mat>> number = myHaarCascade.DetectNumber(Position, licensePlate);
 
 				SVMModel myClassify;
 				for (int i = 0; i < number.size(); i++)
 				{
+					for (int j = 0; j < number.at(i).size(); j++)
+					{
+						imshow("num", number[i][j]);
+						waitKey(0);
+					}
 					myClassify.testing(number.at(i));
 				}
 
-				//dir = "D:/FPT learn/TEST/a";
-				//dir.append(to_string(i));
-				//dir.append(".jpg");
-				//imwrite(dir, myHaarCascade.DectectLicensePlate(image));
 				imshow("licensePlate", licensePlate);
 				waitKey(0);
 			}
